5a1cd5778480b7dce7e4f5bfa0d27fbbca819620
Yoki Jati Perkasa <yokijatiforwork@gmail.com>
2023-11-25 23:25:55 +0700

    Sukses menambahkan fitur search bar di explore fragment

--- app/src/main/java/com/enigma/discoverbatik/view/fragment/explore/ExploreFragment.kt
+++ app/src/main/java/com/enigma/discoverbatik/view/fragment/explore/ExploreFragment.kt
@@ -1,15 +1,20 @@
 package com.enigma.discoverbatik.view.fragment.explore
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.enigma.discoverbatik.R
-import com.enigma.discoverbatik.view.fragment.home.HomeFragment
 
 
 class ExploreFragment : Fragment(), View.OnClickListener {
+
+    private lateinit var editText: EditText
+    private lateinit var textView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +26,26 @@ class ExploreFragment : Fragment(), View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        editText = view.findViewById(R.id.search_bar)
+        textView = view.findViewById(R.id.hello_world)
+
+        editText.setOnKeyListener { _, keyCode, event ->
+
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                handleTextSubmission()
+                true
+            } else {
+                false
+            }
+
+        }
+
+    }
+
+    private fun handleTextSubmission() {
+        val enteredText = editText.text.toString()
+        textView.text = enteredText
     }
 
     override fun onClick(v: View?) {
